4aae55585bb462036acdf8f6f96b90ce3dbbd3ec
JoaoJuliasz <joaojuliaszdemorais@gmail.com>
2023-07-17 09:27:02 -0300

    add validation for not found pokemons

--- src/main/java/com/looqbox/web/PokemonController.java
+++ src/main/java/com/looqbox/web/PokemonController.java
@@ -4,12 +4,15 @@ import com.looqbox.persistence.model.Pokemon;
 import com.looqbox.service.IPokemonService;
 import com.looqbox.sorting.SortTypes;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -24,14 +27,26 @@ public class PokemonController {
 
     @GetMapping
     public Collection<String> findAll(@RequestParam(required = false) Optional<String> query, @RequestParam(required = false) Optional<SortTypes> sort) {
-        return pokemonService.findAll(query, sort);
+
+        List<String> foundPokemons = pokemonService.findAll(query, sort);
+
+        if (foundPokemons.size() == 0) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return foundPokemons;
     }
 
     @GetMapping(value = "/highlight")
     public Collection<Pokemon> findAllHighlight(
             @RequestParam(required = false) Optional<String> query,
             @RequestParam(name = "sort", required = false, defaultValue = "alphabetical") Optional<SortTypes> sort) {
-        return pokemonService.findAllHighlight(query, sort);
-    }
 
+        List<Pokemon> foundPokemons = pokemonService.findAllHighlight(query, sort);
+
+        if (foundPokemons.size() == 0) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+        return foundPokemons;
+    }
 }
